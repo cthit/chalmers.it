0b3b126d8f0fc32e715b4fb8cdda4c6352eba028
Oscar Eriksson <oscariremma@gmail.com>
2023-09-25 18:57:57 +0200

    Utoipa OpenAPI

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,22 @@
-use axum::routing::get;
+use std::sync::Arc;
+
+use axum::{extract::State, routing::get};
 use dotenv::dotenv;
 use http::StatusCode;
 use tower_request_id::RequestIdLayer;
 use tracing::info;
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
 
 mod logging;
+
+struct AppState {
+    name: String,
+}
+
+#[derive(OpenApi)]
+#[openapi(paths(hello_world))]
+struct ApiDoc;
 
 #[tokio::main]
 async fn main() {
@@ -15,10 +27,14 @@ async fn main() {
     let trace_layer = logging::http::trace_layer();
 
     let app = axum::Router::new()
+        .merge(SwaggerUi::new("/api/swagger").url("/api/v1/openapi.json", ApiDoc::openapi()))
         .route("/", get(hello_world))
         .route("/fail", get(fail))
         .layer(trace_layer)
-        .layer(RequestIdLayer);
+        .layer(RequestIdLayer)
+        .with_state(Arc::new(AppState {
+            name: "Hello, world!".into(),
+        }));
 
     info!("Starting server on 0.0.0.0:3000");
     axum::Server::bind(
@@ -31,8 +47,9 @@ async fn main() {
     .expect("Axum crashed.")
 }
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+#[utoipa::path(get, path = "/", responses((status = 200, description = "Todo item created successfully", body = String),))]
+async fn hello_world(State(state): State<Arc<AppState>>) -> String {
+    state.name.clone()
 }
 
 async fn fail() -> Result<String, (StatusCode, String)> {
