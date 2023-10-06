2b261cb2b8d2ae9bbd9d36908a811b018b6d3dc8
Goostaf <gasplund2@gmail.com>
2023-10-06 22:59:55 +0200

    Test cleanup

--- backend/api/v1/news.go
+++ backend/api/v1/news.go
@@ -3,8 +3,6 @@ package apiv1
 import (
 	"net/http"
 
-	"github.com/charmbracelet/log"
-
 	"github.com/go-chi/chi/v5"
 )
 
@@ -22,9 +20,5 @@ func News() chi.Router {
 //	@Success		200	{object}	string
 //	@Router			/news [get]
 func getNews(w http.ResponseWriter, r *http.Request) {
-	log.Warn("This is a warning")
-	log.Debug("This is a debug message")
-	log.Info("This is an info message")
-	log.Error("This is an error message")
-	w.Write([]byte("['Imagine that this is a list of news posts']"))
+	w.Write([]byte("[]"))
 }
