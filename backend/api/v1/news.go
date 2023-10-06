d7e5f2cbf279e0aacf86e3634340b88a85fddadc
Goostaf <gasplund2@gmail.com>
2023-10-06 23:04:03 +0200

    Make internal routers private

--- backend/api/v1/news.go
+++ backend/api/v1/news.go
@@ -6,7 +6,7 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func News() chi.Router {
+func newsRouter() chi.Router {
 	r := chi.NewRouter()
 	r.Get("/", getNews)
 	return r
--- backend/api/v1/router.go
+++ backend/api/v1/router.go
@@ -4,6 +4,6 @@ import "github.com/go-chi/chi/v5"
 
 func Router() chi.Router {
 	r := chi.NewRouter()
-	r.Mount("/news", News())
+	r.Mount("/news", newsRouter())
 	return r
 }
