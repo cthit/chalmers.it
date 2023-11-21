ad07f461b69a7a9c5bd31d041c42229239d496bd
GAsplund <gasplund2@gmail.com>
2023-11-21 12:29:21 +0100

    User login state

--- src/components/Header/User/User.tsx
+++ src/components/Header/User/User.tsx
@@ -1,17 +1,33 @@
 import styles from './User.module.scss';
 
-const Header = () => {
+const User = () => {
+  const isLogged: boolean = true;
+
   return (
     <div className={styles.user}>
       <img src="/smurf.svg" className={styles.pfp} />
-      <div className={styles.name}>
-        <p>User</p>
-        <a href="/settings">
-          <img src="/settings.svg" />
-        </a>
-      </div>
+      {isLogged ? <LoggedIn /> : <NotLogged />}
     </div>
   );
 };
 
-export default Header;
+const LoggedIn = () => {
+  return (
+    <div className={styles.name}>
+      <p>User</p>
+      <a href="https://gamma.chalmers.it/me/edit">
+        <img src="/settings.svg" />
+      </a>
+    </div>
+  );
+};
+
+const NotLogged = () => {
+  return (
+    <div className={styles.name}>
+      <a>Logga in</a>
+    </div>
+  );
+};
+
+export default User;
