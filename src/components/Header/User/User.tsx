dd5246815d4c3d1afc7ec9dce9d10581dabe9115
Goostaf <gasplund2@gmail.com>
2023-11-23 14:20:44 +0100

    Login button

--- src/components/Header/User/User.tsx
+++ src/components/Header/User/User.tsx
@@ -1,34 +1,31 @@
+import ActionButton from '@/components/ActionButton/ActionButton';
 import styles from './User.module.scss';
-import { FaCog } from "react-icons/fa";
+import { FaCog } from 'react-icons/fa';
 
 const User = () => {
   const isLogged: boolean = true;
 
   return (
-    <div className={styles.user}>
-      <img src="/smurf.svg" className={styles.pfp} />
-      {isLogged ? <LoggedIn /> : <NotLogged />}
-    </div>
+    <div className={styles.user}>{isLogged ? <LoggedIn /> : <NotLogged />}</div>
   );
 };
 
 const LoggedIn = () => {
   return (
-    <div className={styles.name}>
-      <p>ALongUsernameForTesting</p>
-      <a href="https://gamma.chalmers.it/me/edit">
-        <FaCog className={styles.settings} />
-      </a>
-    </div>
+    <>
+      <img src="/smurf.svg" className={styles.pfp} />
+      <div className={styles.name}>
+        <p>ALongUsernameForTesting</p>
+        <a href="https://gamma.chalmers.it/me/edit">
+          <FaCog className={styles.settings} />
+        </a>
+      </div>
+    </>
   );
 };
 
 const NotLogged = () => {
-  return (
-    <div className={styles.name}>
-      <a>Logga in</a>
-    </div>
-  );
+  return <ActionButton href="/login">Logga in</ActionButton>;
 };
 
 export default User;
