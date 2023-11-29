d8f997e477145addea8af77423adc89fd0d083cf
Goostaf <gasplund2@gmail.com>
2023-11-29 11:41:21 +0100

    Workflow for building & testing, make prisma build
    Install prettier
    
    Try using Reviewdog
    
    Use individual actions
    
    Install node beforehand
    
    Don't install globally
    
    Set filter mode
    
    Underscore
    
    Use database service
    
    Use better Prisma object
    
    Make API routes build
    
    Fix typo, remove unnecessary service
    
    Use node version env
    
    Make tests work
    
    Refer to versions properly

--- src/services/divisionGroupService.ts
+++ src/services/divisionGroupService.ts
@@ -1,4 +1,4 @@
-import { prisma } from '@/prisma';
+import prisma from '@/prisma';
 
 export default class DivisionGroupService {
   static async getAll() {
--- src/services/mediaService.ts
+++ src/services/mediaService.ts
@@ -1,4 +1,4 @@
-import { prisma } from '@/prisma';
+import prisma from '@/prisma';
 import { readFile, writeFile } from 'fs/promises';
 
 // TODO: Use a config down the line
