196383bdfb5c687f13f5ba6e49ad0a64cfd10e96
Tussies <86693581+Tussies@users.noreply.github.com>
2023-12-03 23:42:18 +0100

    added a simple search bar

--- src/components/Header/SearchBar/Search.tsx
+++ src/components/Header/SearchBar/Search.tsx
@@ -1,13 +1,29 @@
+'use client';
 
+import React, { useState } from 'react';
 import styles from './Search.module.scss';
-import { BsSearchHeartFill } from "react-icons/bs";
+import { BsSearchHeartFill } from 'react-icons/bs';
 
 const Search = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleIconClick = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div className={styles.search}>
-        <div> <BsSearchHeartFill /> </div>
+    <div className={styles.searchBarContainer}>
+      <BsSearchHeartFill className={styles.searchIcon} onClick={handleIconClick} />
+      {isExpanded && (
+        <input
+          className={styles.expandedSearchBar}
+          type="text"
+          placeholder="Search..."
+        />
+      )}
     </div>
   );
 };
 
 export default Search;
+
