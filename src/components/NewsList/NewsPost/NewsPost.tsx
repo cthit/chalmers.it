8539a6c63b3dbe8c439b53f33081fb0cc6075265
GAsplund <gasplund2@gmail.com>
2023-12-05 17:52:45 +0100

    Post links

--- src/components/NewsList/NewsPost/NewsPost.tsx
+++ src/components/NewsList/NewsPost/NewsPost.tsx
@@ -1,5 +1,6 @@
 import style from "./NewsPost.module.scss";
 import { Prisma } from "@prisma/client";
+import Link from "next/link";
 import { FC } from "react";
 
 interface ActionButtonProps {
@@ -20,7 +21,7 @@ interface ActionButtonProps {
 const NewsPost: FC<ActionButtonProps> = ({ post }) => {
     return (
       <div className={style.post}>
-        <h2>{post.titleSv}</h2>
+        <h2><Link href={`/post/${post.id}`}>{post.titleSv}</Link></h2>
         <p className={style.subtitle}>Publicerad {post.createdAt.toLocaleString()} {post.divisionGroupId != null && `för ${post.divisionGroupId}`} av {post.writtenByCid}</p>
         <p>{post.contentSv}</p>
       </div>
