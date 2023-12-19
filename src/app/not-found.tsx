64c43f2504495eb3f250d4b5e0fcc17fe335c4a2
Goostaf <gasplund2@gmail.com>
2023-12-19 01:14:34 +0100

    Add contact link

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,27 @@
-import Link from 'next/link'
+import Link from 'next/link';
 import styles from './not-found.module.scss';
 import Image from 'next/image';
- 
+
 export default function NotFound() {
   return (
     <div className={styles.content}>
       <h1 className={styles.header}>404</h1>
-      <Image src="/404.png" alt={''} width={300} height={261} />
-      <p>This is not the site you're looking for!</p>
-      <Link href="/">Return Home</Link>
+      <Image
+        className={styles.image}
+        src="/404.png"
+        alt={''}
+        width={300}
+        height={261}
+      />
+      <p>
+        This is not the site you're looking for!
+        <br />
+        If you believe it is, please contact{' '}
+        <a href="mailto:digit@chalmers.it">digit@chalmers.it</a>
+      </p>
+      <Link className={styles.homeLink} href="/">
+        Return Home
+      </Link>
     </div>
-  )
+  );
 }
