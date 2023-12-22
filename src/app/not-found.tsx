938ac8dc841870ef45f03dc59ac230aca79f4aef
Goostaf <gasplund2@gmail.com>
2023-12-22 18:28:25 +0100

    Use picture element, add proper alt text

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,24 +1,31 @@
 import Link from 'next/link';
 import styles from './error.module.scss';
-import Image from 'next/image';
 
 export default function NotFound() {
   return (
     <div className={styles.content}>
       <h1 className={styles.header}>404</h1>
-      <Image
-        className={styles.image}
-        src="/404.png"
-        alt={''}
-        width={300}
-        height={261}
-      />
+
+      <picture>
+        <img
+          className={styles.image}
+          src="/404.png"
+          alt="Obi-Wan Kenobi using the Jedi mind trick"
+        />
+      </picture>
+
       <p>
         {"This is not the site you're looking for!"}
         <br />
         If you believe it is, please contact{' '}
-        <a className={styles.actionLink} href="mailto:digit@chalmers.it">digit@chalmers.it</a> or{' '}
-        <a className={styles.actionLink} href="https://github.com/cthit/chalmers.it/issues/new/choose">
+        <a className={styles.actionLink} href="mailto:digit@chalmers.it">
+          digit@chalmers.it
+        </a>{' '}
+        or{' '}
+        <a
+          className={styles.actionLink}
+          href="https://github.com/cthit/chalmers.it/issues/new/choose"
+        >
           create an issue
         </a>
       </p>
