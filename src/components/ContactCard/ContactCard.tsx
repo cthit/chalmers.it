a1b968ce588750ce4a7118ea68736187407e9491
Goostaf <gasplund2@gmail.com>
2024-04-27 15:55:59 +0200

    Add basic locale switching

--- src/components/ContactCard/ContactCard.tsx
+++ src/components/ContactCard/ContactCard.tsx
@@ -1,18 +1,35 @@
+'use client';
+
+import { usePathname, useRouter } from 'next/navigation';
 import Link from 'next/link';
 import Divider from '../Divider/Divider';
 import styles from './ContactCard.module.scss';
+import i18nService from '@/services/i18nService';
 
-const ContactCard = () => {
+const ContactCard = ({ locale }: { locale: string }) => {
+  const router = useRouter();
+  const currentPathname = usePathname();
+
+  const l = i18nService.getLocale(locale);
+
+  const switchLocale = (toSv: boolean) => {
+    if (currentPathname.startsWith('/en') && toSv) {
+      router.push(currentPathname.replace(`/en`, `/sv`));
+    } else if (!currentPathname.startsWith('/en') && !toSv) {
+      router.push(`/en${currentPathname}`);
+    }
+  };
+
   return (
     <div className={styles.contactCard}>
       <div className={styles.languageToggle}>
-        <a className={styles.button}>
+        <a onClick={() => switchLocale(true)} className={styles.button}>
           <picture className={styles.flag}>
             <img src="/sweden.svg" alt="Swedish flag" />
           </picture>
           <p>Svenska</p>
         </a>
-        <a className={styles.button}>
+        <a onClick={() => switchLocale(false)} className={styles.button}>
           <picture className={styles.flag}>
             <img src="/uk.svg" alt="English flag" />
           </picture>
@@ -21,26 +38,26 @@ const ContactCard = () => {
       </div>
       <Divider />
       <p>
-        <strong>POSTADRESS</strong>
+        <strong>{l.footer.postadress}</strong>
       </p>
       <p>Teknologsektionen Informationsteknik</p>
-      <p>Toknologgården 2</p>
+      <p>Teknologgården 2</p>
       <p>412 58 Göteborg</p>
       <br />
       <p>
-        <strong>BESÖKSADDRESS</strong>
+        <strong>{l.footer.visitadress}</strong>
       </p>
       <p>Hubben 2.2</p>
       <p>Hörsalsvägen 9</p>
       <p>412 58 Göteborg</p>
       <br />
       <p>
-        <strong>KONTAKT</strong>
+        <strong>{l.footer.contact}</strong>
       </p>
       <p>styrit@chalmers.it</p>
       <Divider />
       <p>
-        Utvecklad av{' '}
+        {l.footer.developed}{' '}
         <Link className={styles.link} href="https://digit.chalmers.it">
           digIT
         </Link>{' '}
@@ -49,7 +66,7 @@ const ContactCard = () => {
           className={styles.link}
           href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
         >
-          mer info
+          {l.footer.moreinfo}
         </Link>{' '}
       </p>
     </div>
