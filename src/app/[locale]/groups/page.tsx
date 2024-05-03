8d8875d33d357e95b4fe80e72bdd71f2c171d886
Goostaf <gasplund2@gmail.com>
2024-05-03 12:15:55 +0200

    Groups list locale

--- src/app/[locale]/groups/page.tsx
+++ src/app/[locale]/groups/page.tsx
@@ -2,14 +2,20 @@ import DivisionGroupService from '@/services/divisionGroupService';
 import styles from './page.module.scss';
 import Link from 'next/link';
 import ContentPane from '@/components/ContentPane/ContentPane';
+import i18nService from '@/services/i18nService';
 
-export default async function Groups() {
+export default async function Groups({
+  params: { locale }
+}: {
+  params: { locale: string };
+}) {
   const groups = await DivisionGroupService.getAll();
+  const l = i18nService.getLocale(locale);
 
   return (
     <main className={styles.main}>
       <ContentPane>
-        <h1>Kommitter, föreningar och andra instanser</h1>
+        <h1>{l.pages.groups}</h1>
         <ul className={styles.groupList}>
           {groups.map((group) => (
             <li key={group.id}>
