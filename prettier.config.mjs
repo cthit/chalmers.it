020163627f848c41669ee9550ef2ae9a5ffc2ae9
Goostaf <gasplund2@gmail.com>
2024-05-29 23:59:51 +0200

    Use ES modules for Next and Prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+export default {
+  arrowParens: 'always',
+  singleQuote: true,
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: true,
+  printWidth: 80
+};
