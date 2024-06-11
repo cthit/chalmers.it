71621d8360ad1a265e1ab3804d75e08b748c477f
Goostaf <gasplund2@gmail.com>
2024-06-11 22:50:01 +0200

    Hide search button for release

--- src/components/Header/SearchSwitcher/SearchSwitcher.tsx
+++ src/components/Header/SearchSwitcher/SearchSwitcher.tsx
@@ -1,11 +1,11 @@
 'use client';
 
 import SearchBar from '../SearchBar/SearchBar';
-import SearchIcon from '../SearchBar/SearchIcon';
+// import SearchIcon from '../SearchBar/SearchIcon';
 import styles from '../Header.module.scss';
 import EscapeHatch from '../EscapeHatch/EscapeHatch';
 import Navigation from '../Navigation/Navigation';
-import { useState } from 'react';
+//import { useState } from 'react';
 
 const SearchSwitcher = ({
   children,
@@ -14,18 +14,18 @@ const SearchSwitcher = ({
   children: React.ReactNode;
   locale: string;
 }) => {
-  const [showSearch, setShowSearch] = useState(false);
+  //const [showSearch, setShowSearch] = useState(false);
 
   return (
     <>
       <EscapeHatch locale={locale} />
-      {showSearch ? (
+      {/*showSearch*/ false ? (
         <SearchBar />
       ) : (
         <Navigation locale={locale} desktop={true} />
       )}
       <div className={styles.right}>
-        <SearchIcon onClick={() => setShowSearch(!showSearch)} />
+        {/*<SearchIcon onClick={() => setShowSearch(!showSearch)} />*/}
         {children}
       </div>
     </>
