07587ebc48941b488fa757d3fafdb5508ccb4f6e
Goostaf <gasplund2@gmail.com>
2024-06-23 18:54:54 +0200

    Get locale for layout on 404 (#92)

--- src/app/[...not-found]/layout.tsx
+++ src/app/[...not-found]/layout.tsx
@@ -1,19 +1,25 @@
-import Layout from '@/app/[locale]/layout';
+import Layout, { generateMetadata as genMeta } from '@/app/[locale]/layout';
+import { cookies } from 'next/headers';
 
-export const metadata = {
-  title: '404 - Page not found',
-  description: 'Software Engineering at Chalmers'
-};
+export function generateMetadata() {
+  const locale = cookies().get('NEXT_LOCALE')?.value ?? 'en';
+  return genMeta({
+    params: {
+      locale
+    }
+  });
+}
 
 export default function RootLayout({
   children
 }: {
   children: React.ReactNode;
 }) {
+  const locale = cookies().get('NEXT_LOCALE')?.value ?? 'en';
   return (
     <Layout
       params={{
-        locale: 'en'
+        locale
       }}
     >
       {children}
