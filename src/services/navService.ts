3430184f1e3d41243db5c5f1ec259a49974e20d2
Goostaf <gasplund2@gmail.com>
2024-08-26 23:56:38 +0200

    Add schema and service

---
+++ src/services/navService.ts
@@ -0,0 +1,93 @@
+import prisma from '@/prisma';
+
+export default class NavService {
+  static async get() {
+    return await prisma.navbarCategory.findMany({
+      include: {
+        NavbarItem: true
+      }
+    });
+  }
+
+  static async addCategory(nameEn: string, nameSv: string, priority?: number) {
+    return await prisma.navbarCategory.create({
+      data: {
+        nameEn,
+        nameSv,
+        priority
+      }
+    });
+  }
+
+  static async addItem(
+    categoryId: number,
+    nameEn: string,
+    nameSv: string,
+    url: string,
+    priority?: number
+  ) {
+    return await prisma.navbarItem.create({
+      data: {
+        nameEn,
+        nameSv,
+        url,
+        priority,
+        categoryId
+      }
+    });
+  }
+
+  static async removeCategory(id: number) {
+    return await prisma.navbarCategory.delete({
+      where: {
+        id
+      }
+    });
+  }
+
+  static async removeItem(id: number) {
+    return await prisma.navbarItem.delete({
+      where: {
+        id
+      }
+    });
+  }
+
+  static async updateCategory(
+    id: number,
+    nameEn: string,
+    nameSv: string,
+    priority?: number
+  ) {
+    return await prisma.navbarCategory.update({
+      where: {
+        id
+      },
+      data: {
+        nameEn,
+        nameSv,
+        priority
+      }
+    });
+  }
+
+  static async updateItem(
+    id: number,
+    nameEn: string,
+    nameSv: string,
+    url: string,
+    priority?: number
+  ) {
+    return await prisma.navbarItem.update({
+      where: {
+        id
+      },
+      data: {
+        nameEn,
+        nameSv,
+        url,
+        priority
+      }
+    });
+  }
+}
