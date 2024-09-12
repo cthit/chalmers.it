3ce9f1ee368d945e2d09fcad6c37b9598185c577
Simon Westlin Green <simon.green@live.se>
2024-09-12 19:10:15 +0200

    Working collapsible content pane, drop in replacement for things that should be collapsible, however it still needs some styling

--- src/components/Lunch/Lunch.tsx
+++ src/components/Lunch/Lunch.tsx
@@ -1,33 +1,30 @@
 import LunchService from '@/services/lunchService';
-import ContentPane from '../ContentPane/ContentPane';
 import Divider from '../Divider/Divider';
 import styles from './Lunch.module.scss';
 import i18nService from '@/services/i18nService';
-import Collapsible from '../Collapsible/Collapsible';
+import ContentPaneCollapsible from '../ContentPane/ContentPaneCollapsible';
 
 const Lunch = async ({ locale }: { locale: string }) => {
   const lunch = await LunchService.getLunch();
   const l = i18nService.getLocale(locale);
   const en = locale === 'en';
   return (
-    <ContentPane>
-      <Collapsible>
-        <h1>Lunch</h1>
-        <Divider />
-        {lunch.length === 0 && <p>{l.lunch.nolunch}</p>}
-        {lunch.map((restaurant) => (
-          <div key={restaurant.name}>
-            <h2>{restaurant.name}</h2>
-            {(en ? restaurant.meals.en : restaurant.meals.sv).map((meal) => (
-              <div key={meal.title}>
-                <h3>{meal.title}</h3>
-                <p>{meal.summary}</p>
-              </div>
-            ))}
-          </div>
-        ))}
-      </Collapsible>
-    </ContentPane>
+    <ContentPaneCollapsible>
+      <h1>Lunch</h1>
+      <Divider />
+      {lunch.length === 0 && <p>{l.lunch.nolunch}</p>}
+      {lunch.map((restaurant) => (
+        <div key={restaurant.name}>
+          <h2>{restaurant.name}</h2>
+          {(en ? restaurant.meals.en : restaurant.meals.sv).map((meal) => (
+            <div key={meal.title}>
+              <h3>{meal.title}</h3>
+              <p>{meal.summary}</p>
+            </div>
+          ))}
+        </div>
+      ))}
+    </ContentPaneCollapsible>
   );
 };
 
