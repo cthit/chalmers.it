1e12e6ac839ab938ce6198553ab51b9c41ec7774
Goostaf <gasplund2@gmail.com>
2024-09-15 15:40:30 +0200

    Better wording

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
-  description = "This Nix flake creates a development shell for the Customer API that provides the
-  required versions of dependencies such as NodeJS 18 and npm 10";
+  description = "This Nix flake creates a development shell that provides a Node.js environment with Prisma and pnpm installed";
   inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
