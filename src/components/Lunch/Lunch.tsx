3c318955120ae3f7d1464cfca505c3c66d0ec892
Simon Westlin Green <simon.green@live.se>
2024-09-26 18:07:38 +0200

    Moved collapsible into ContentPane and changed how it's used in Lunch.tsx

--- src/components/Lunch/Lunch.tsx
+++ src/components/Lunch/Lunch.tsx
@@ -1,29 +1,32 @@
 import LunchService from '@/services/lunchService';
 import Divider from '../Divider/Divider';
 import i18nService from '@/services/i18nService';
-import ContentPaneCollapsible from '../ContentPane/ContentPaneCollapsible';
+import Collapsible from '../ContentPane/Collapsible/Collapsible';
+import ContentPane from '../ContentPane/ContentPane';
 
 const Lunch = async ({ locale }: { locale: string }) => {
   const lunch = await LunchService.getLunch();
   const l = i18nService.getLocale(locale);
   const en = locale === 'en';
   return (
-    <ContentPaneCollapsible>
-      <h1>Lunch</h1>
-      <Divider />
-      {lunch.length === 0 && <p>{l.lunch.nolunch}</p>}
-      {lunch.map((restaurant) => (
-        <div key={restaurant.name}>
-          <h2>{restaurant.name}</h2>
-          {(en ? restaurant.meals.en : restaurant.meals.sv).map((meal) => (
-            <div key={meal.title}>
-              <h3>{meal.title}</h3>
-              <p>{meal.summary}</p>
-            </div>
-          ))}
-        </div>
-      ))}
-    </ContentPaneCollapsible>
+    <ContentPane>
+      <Collapsible>
+        <h1>Lunch</h1>
+        <Divider />
+        {lunch.length === 0 && <p>{l.lunch.nolunch}</p>}
+        {lunch.map((restaurant) => (
+          <div key={restaurant.name}>
+            <h2>{restaurant.name}</h2>
+            {(en ? restaurant.meals.en : restaurant.meals.sv).map((meal) => (
+              <div key={meal.title}>
+                <h3>{meal.title}</h3>
+                <p>{meal.summary}</p>
+              </div>
+            ))}
+          </div>
+        ))}
+      </Collapsible>
+    </ContentPane>
   );
 };
 
