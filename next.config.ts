393f416ed4a45b857116210ca6758809fd9bf521
Oscar Eriksson <oscariremma@gmail.com>
2024-12-11 20:11:51 +0100

    Use TS for next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { NextConfig } from 'next';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-/** @type {import('next').NextConfig} */
-const config = {
+const config: NextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
     prependData: `@use "@/styles/variables.scss" as *;`
