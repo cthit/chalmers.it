af59b0a22d419ce86238037f228699d8b4fb7cb0
Goostaf <gasplund2@gmail.com>
2025-09-16 22:04:18 +0200

    Use MDXEditor

--- src/components/MarkdownEditor/MarkdownEditor.tsx
+++ src/components/MarkdownEditor/MarkdownEditor.tsx
@@ -1,11 +1,65 @@
+'use client';
+
 import style from './MarkdownEditor.module.scss';
 import { TextareaHTMLAttributes } from 'react';
+import {
+  BlockTypeSelect,
+  BoldItalicUnderlineToggles,
+  CodeToggle,
+  CreateLink,
+  diffSourcePlugin,
+  DiffSourceToggleWrapper,
+  headingsPlugin,
+  imagePlugin,
+  InsertImage,
+  linkDialogPlugin,
+  linkPlugin,
+  listsPlugin,
+  markdownShortcutPlugin,
+  MDXEditor,
+  quotePlugin,
+  thematicBreakPlugin,
+  toolbarPlugin
+} from '@mdxeditor/editor';
+import '@mdxeditor/editor/style.css';
 
 const MarkdownEditor = ({
-  className,
-  ...rest
+  className
 }: TextareaHTMLAttributes<HTMLTextAreaElement>) => {
-  return <textarea className={`${style.editor} ${className}`} {...rest} />;
+  return (
+    <MDXEditor
+      markdown=""
+      plugins={[
+        linkPlugin(),
+        linkDialogPlugin(),
+        imagePlugin(),
+        diffSourcePlugin({
+          readOnlyDiff: true
+        }),
+        toolbarPlugin({
+          toolbarContents: () => (
+            <>
+              <DiffSourceToggleWrapper>
+                <BoldItalicUnderlineToggles />
+                <BlockTypeSelect />
+                <CodeToggle />
+                <InsertImage />
+                <CreateLink />
+              </DiffSourceToggleWrapper>
+            </>
+          )
+        }),
+        headingsPlugin(),
+        listsPlugin(),
+        quotePlugin(),
+        thematicBreakPlugin(),
+        markdownShortcutPlugin()
+      ]}
+      className={`${className} ${style.editor}`}
+      contentEditableClassName={style.contentEditable}
+      placeholder={undefined}
+    />
+  );
 };
 
 export default MarkdownEditor;
