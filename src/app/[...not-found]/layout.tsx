4ce7bbb519b0308f0df6a510a07f704892dcb2d8
Goostaf <gasplund2@gmail.com>
2025-10-15 20:39:27 +0200

    Pass promise to layout

--- src/app/[...not-found]/layout.tsx
+++ src/app/[...not-found]/layout.tsx
@@ -2,29 +2,21 @@ import Layout, { generateMetadata as genMeta } from '@/app/[locale]/layout';
 import { cookies } from 'next/headers';
 
 export async function generateMetadata() {
-  const locale = (await cookies()).get('NEXT_LOCALE')?.value ?? 'en';
+  const locale = cookies().then((c) => ({
+    locale: c.get('NEXT_LOCALE')?.value ?? 'en'
+  }));
   return genMeta({
-    params: {
-      locale
-    }
+    params: locale
   });
 }
 
-export default async function RootLayout(
-  {
-    children
-  }: {
-    children: React.ReactNode;
-  }
-) {
-  const locale = (await cookies()).get('NEXT_LOCALE')?.value ?? 'en';
-  return (
-    <Layout
-      params={{
-        locale
-      }}
-    >
-      {children}
-    </Layout>
-  );
+export default async function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  const locale = cookies().then((c) => ({
+    locale: c.get('NEXT_LOCALE')?.value ?? 'en'
+  }));
+  return <Layout params={locale}>{children}</Layout>;
 }
