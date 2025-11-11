7d5deb4eb7bde5feb9224ec5cee1b71361dab94f
Goostaf <gasplund2@gmail.com>
2025-11-11 18:56:56 +0100

    Custom dirname

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,30 @@
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __custom_filename = fileURLToPath(import.meta.url);
+const __custom_dirname = dirname(__custom_filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __custom_dirname
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     rules: {
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "warn",
-      "no-unused-vars": "off",
-      "prefer-const": "warn",
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'warn',
+      'no-unused-vars': 'off',
+      'prefer-const': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
         {
-          argsIgnorePattern: "^_",
-        },
-      ],
-    },
-  },
+          argsIgnorePattern: '^_'
+        }
+      ]
+    }
+  }
 ];
 
 export default eslintConfig;
