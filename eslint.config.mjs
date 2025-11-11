36d2ba2b81218c8dca0226499fb5ec82dc83cfb6
Goostaf <gasplund2@gmail.com>
2025-11-11 18:55:04 +0100

    Attempt to fix ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "warn",
+      "no-unused-vars": "off",
+      "prefer-const": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
+
+export default eslintConfig;
