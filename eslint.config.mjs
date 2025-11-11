6bff12ec6ab7f36086cad5ad638c23d7746aa58e
Goostaf <gasplund2@gmail.com>
2025-11-11 19:28:30 +0100

    Fix eslint probably

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,36 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
 
-const __custom_filename = fileURLToPath(import.meta.url);
-const __custom_dirname = dirname(__custom_filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __custom_dirname
-});
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+export default [
   {
+    ignores: ['.next/**', 'node_modules/**', 'out/**', 'build/**', 'db/**', 'media/**']
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true }
+      }
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      '@typescript-eslint': typescriptEslint
+    },
+    settings: {
+      react: { version: 'detect' }
+    },
     rules: {
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'warn',
       'no-unused-vars': 'off',
       'prefer-const': 'warn',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_'
-        }
-      ]
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
     }
   }
 ];
-
-export default eslintConfig;
