f2beb57cd046ac93b8827bcdbf1ea08b83a9077b
Goostaf <gasplund2@gmail.com>
2025-11-11 18:46:40 +0100

    Use Next.JS 16

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import { NextConfig } from 'next';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const config: NextConfig = {
   sassOptions: {
