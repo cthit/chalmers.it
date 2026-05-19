9cb16f10602f0599a2ef038983d48ff9828760df
Goostaf <gasplund2@gmail.com>
2026-05-19 19:58:10 +0200

    Titles for pages and news posts

--- src/app/[locale]/(groups)/groups/[id]/layout.tsx
+++ src/app/[locale]/(groups)/groups/[id]/layout.tsx
@@ -1,28 +1,6 @@
 import Banner from '@/components/Banner/Banner';
 import DivisionGroupService from '@/services/divisionGroupService';
 import GammaService from '@/services/gammaService';
-import { Metadata } from 'next';
-import i18nService from '@/services/i18nService';
-
-export async function generateMetadata(props: {
-  params: Promise<{ locale: string; id: string }>;
-}) {
-  const params = await props.params;
-
-  const { locale, id } = params;
-
-  const group = await DivisionGroupService.getInfoBySlug(id).catch(() => {
-    return null;
-  });
-  const l = i18nService.getLocale(locale);
-  return {
-    title:
-      group !== null
-        ? group.prettyName
-        : undefined,
-    description: l.site.siteDescription
-  } as Metadata;
-}
 
 export default async function GroupLayout(props: {
   children: React.ReactNode;
--- src/app/[locale]/(main-content)/pages/[...slug]/page.tsx
+++ src/app/[locale]/(main-content)/pages/[...slug]/page.tsx
@@ -2,13 +2,30 @@ import DivisionNavigation from '@/components/DivisionNavigation/DivisionNavigati
 import ThreePaneLayout from '@/components/ThreePaneLayout/ThreePaneLayout';
 import DivisionPage from '@/components/DivisionPage/DivisionPage';
 import ContactCard from '@/components/ContactCard/ContactCard';
+import DivisionPageService from '@/services/divisionPageService';
+import { notFound } from 'next/navigation';
+
+export async function generateMetadata(props: {
+  params: Promise<{ locale: string; slug: string[] }>;
+}) {
+  const { locale, slug } = await props.params;
+
+  const page = await DivisionPageService.getBySlug(slug);
+  if (page === undefined) {
+    notFound();
+  }
+
+  const isEn = locale === 'en';
+
+  return {
+    title: isEn ? page.titleEn : page.titleSv,
+  };
+}
 
 export default async function Page(props: {
   params: Promise<{ locale: string; slug: string[] }>;
 }) {
-  const params = await props.params;
-
-  const { locale, slug } = params;
+  const { locale, slug } = await props.params;
 
   const main = DivisionPage(locale, slug);
   const left = (
